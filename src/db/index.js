8a1aaab2bd59162db074e79eaa59c09699127fba
sgod112 <hackgodsk112@gmail.com>
2024-05-03 16:20:33 +0530

    git

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ReactEmployee')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Mongo is connected");
     } catch (error) {
         console.error(error);
