9c29b300095d69e108fe152824761651a41e972a
sgod112 <hackgodsk112@gmail.com>
2024-05-03 16:27:15 +0530

    change

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
@@ -19,7 +18,7 @@ app.use(express.static('./public'))
 import employee from './routes/employee.routes.js'
 import timeAttendence from './routes/time_Attendence.routes.js'
 
-
+app.use('/', (req, res) => { res.json({ message: "deployed" }) })
 app.use('/api/employee', employee)
 app.use('/api/timeAtt', timeAttendence)
 
